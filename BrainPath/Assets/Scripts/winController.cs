7827c778e812f07f52b0471b7c3b68a022ac3910
Karolina <beliharova@gmail.com>
2018-05-24 13:42:32 +0200

    pexeso

---
+++ BrainPath/Assets/Scripts/winController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class winController : MonoBehaviour {
+    [SerializeField] private int _score;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_score == 10)
+        {
+            //TODO
+        }
+	}
+}
